736201da8e408f73097f7d0312e20980ae321253
Lucas Barbosa Cesar de Oliveira <lucascoliveira819@gmail.com>
2022-06-02 20:51:17 -0400

    Project Part 02 - Project Refactoring and System Core.

--- src/index.js
+++ src/index.js
@@ -1,36 +1,47 @@
-import DraftLog from 'draftlog'
-import chalk from 'chalk'
-import chalkTable from 'chalk-table'
-import readline from 'readline' /// Para obtermos o input do usuário via terminal
 
 import database from './../database.json'
-import Person from './person.js' /// no ECMAScript modules, obrigatoriamente precisamos passar a extensão do arquivo, quando estamos trabahlando com vários tipos de extensão, se estiver trabalhando só com arquivos js, não precisa passar a extensão nos imports 
+import Person from './person.js'
 
-DraftLog(console).addLineListener(process.stdin) /// Adiciona um listener no terminal para o sistema receber os comandos
+import TerminalController from './terminalController.js';
+
 const DEFAULT_LANG = 'pt-BR'
+const STOP_TERM = ":q"
+const MINIMUM_LENGTH = 30
 
-const options = {
-  leftPad: 2,
-  columns: [
-    { field: "id", name: chalk.cyan("ID") }, ///chalk irá imprimir na tela o que está entre parenteses na cor de nome da função, no caso cyan
-    { field: "vehicles", name: chalk.magenta("Vehicles") },
-    { field: "kmTraveled", name: chalk.cyan("Km Traveled") },
-    { field: "from", name: chalk.cyan("From") },
-    { field: "to", name: chalk.cyan("To") },
-  ]
+const terminalController = new TerminalController()
+terminalController.initializeTerminal(database, DEFAULT_LANG)
+
+async function mainLoop() {
+  try {
+    const answer = await terminalController.question()
+    if (answer === STOP_TERM) {
+      terminalController.closeTerminal()
+      console.log('process finished!')
+      return
+    }
+
+
+    /// "2 Bike 2 2000-03-04 2002-05-04"
+    /// ['2', 'bike', '2', '2000-03-04', '2002-05-04']
+
+    const EMPTY_SPACE = ' '
+    const organizedData = answer.split(EMPTY_SPACE)
+
+    if (MINIMUM_LENGTH > answer.length || organizedData.length < 5) {
+      console.log('Não há dados suficientes para preencher a tabela')
+      return mainLoop()
+    }
+
+
+    const person = Person.generateInstanceFromString(answer)
+    console.log('person', person.formatted(DEFAULT_LANG))
+
+    return mainLoop()
+
+  } catch (error) {
+    console.error('DEU RUIM**', error)
+    return mainLoop()
+  }
 }
 
-const table = chalkTable(options, database.map(item => new Person(item).formatted(DEFAULT_LANG))) /// Inicializando o chalkTable com as configurações e o database
-const print = console.draft(table) /// inicializando o console.draft -> draft Substitui a instância global do console, na linha 7 instanciamos o DraftLog e ele injetou o draft pra gente
-
-const terminal = readline.createInterface({
-  input: process.stdin, /// stdin é o terminal propriamente dito
-  output: process.stdout /// terminal tbm
-})
-
-// terminal.question('Qual é o seu nome?', msg => {
-//   console.log('msg', msg.toString())
-// })
-
-
-
+await mainLoop()
---
+++ src/terminalController.js
@@ -0,0 +1,58 @@
+
+import readline from 'readline' /// Para obtermos o input do usuário via terminal
+import Person from './person.js'
+import DraftLog from 'draftlog'
+import chalk from 'chalk'
+import chalkTable from 'chalk-table'
+
+
+
+export default class TerminalController {
+  constructor() {
+    this.print = {}
+    this.data = {}
+
+  }
+
+  initializeTerminal(database, language) {
+    DraftLog(console).addLineListener(process.stdin) /// Adiciona um listener no terminal para o sistema receber os comandos
+    this.terminal = readline.createInterface({
+      input: process.stdin, /// stdin é o terminal propriamente dito
+      output: process.stdout /// terminal tbm
+    })
+
+    this.initializeTable(database, language)
+
+  }
+
+  initializeTable(database, language) {
+    const data = database.map(item => new Person(item).formatted(language))
+    const table = chalkTable(this.getTableOptions(), data)
+
+    this.print = console.draft(table)
+    this.data = data
+
+  }
+
+  question(msg = '') {
+    return new Promise(resolve => this.terminal.question(msg, resolve))
+  }
+
+  closeTerminal() {
+    this.terminal.close()
+  }
+
+  getTableOptions() {
+    return {
+      leftPad: 2,
+      columns: [
+        { field: "id", name: chalk.cyan("ID") }, ///chalk irá imprimir na tela o que está entre parenteses na cor de nome da função, no caso cyan
+        { field: "vehicles", name: chalk.magenta("Vehicles") },
+        { field: "kmTraveled", name: chalk.cyan("Km Traveled") },
+        { field: "from", name: chalk.cyan("From") },
+        { field: "to", name: chalk.cyan("To") },
+      ]
+    }
+
+  }
+}
